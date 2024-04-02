976d45340cb7342774d232093ad08f6d7adefa11
Hiren <Hirenshah2550@gmail.com>
2024-04-02 19:32:31 +0530

    Redux basic concept

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { thunk } from "redux-thunk";
 import logger from "redux-logger";
 import axios from "axios";
 
 const init = "intializeAmount";
-function reducer(state = { amount: 10 }, action) {
+const incrementByAmount = "incrementByAmount";
+const errorAmount = "errorAmount";
+function accountReducer(state = { amount: 10 }, action) {
   switch (action.type) {
+    case incrementByAmount:
+      return { amount: state.amount + action.payload };
     case init:
       return { amount: state.amount + action.payload };
+    case errorAmount:
+      return { ...state, error: action.error };
     default:
       return { amount: 10 };
   }
 }
-const store = createStore(reducer, applyMiddleware(logger.default, thunk));
+function bonusReducer(state = { points: 0 }, action) {
+  switch (action.type) {
+    case incrementByAmount:
+      if (action.payload >= 100) {
+        return { points: state.points + 1 };
+      }
+    case init:
+      return { points: 0 };
+    default:
+      return { points: 0 };
+  }
+}
+const store = createStore(
+  combineReducers({ account: accountReducer, bonus: bonusReducer }),
+  applyMiddleware(logger.default, thunk)
+);
 
+function incByAmount(amount) {
+  return { type: incrementByAmount, payload: amount };
+}
 function userAmount(id) {
   return async (dispatch, getState) => {
-    const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
-    dispatch({ type: "intializeAmount", payload: data.amount });
+    try {
+      const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
+      dispatch({ type: "intializeAmount", payload: data.amount });
+    } catch (error) {
+      dispatch({ type: "errorAmount", error: error.message });
+    }
   };
 }
 
@@ -25,4 +53,5 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(userAmount(4));
+// store.dispatch(incByAmount(20));
+store.dispatch(userAmount(2));
